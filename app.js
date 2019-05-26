8945a486cec6104bc9f33fe1915494b91a95e3c9
qq1257787262 <1257787262@qq.com>
2019-05-26 10:47:33 +0800

    完成接口1的逻辑处理

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.use(function (req, res, next) {
     //res.status(404).send("Sorry can't find that!")
   })
 
-app.listen(3000,function(){
+app.listen(4000,function(){
     console.log('go````')
 })
